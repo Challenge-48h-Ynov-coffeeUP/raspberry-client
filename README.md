4ddb326b16eb7426321cf49c2bc7cde3a2f7a7c4
Seddik Mehdi <mhdi.seddik@gmail.com>
2023-04-14 17:08:58 +0200

    feat: init raspberry server node

---
+++ README.md
@@ -0,0 +1,18 @@
+Node.js / typescript / WebSocket server for Y-coffee
+
+## Usage
+You will need nodejs ( at least v16 ) and yarn
+
+In the project directory, you can run:
+
+### `yarn install`
+then
+### `yarn start`
+
+It simply runs the app in the development mode.\
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+If the websocket server is hosted on a different port/ip, you can change it in the .env file
+## example
+REACT_APP_backend=ws://192.168.1.6:8999
---
+++ src/app.ts
@@ -0,0 +1,52 @@
+var WebSocketClient = require("websocket").client;
+
+var client = new WebSocketClient();
+
+client.on("connectFailed", function (error: any) {
+  console.log("Connect Error: " + error.toString());
+});
+
+client.on("connect", function (connection: any) {
+  const readline = require("readline");
+  readline.emitKeypressEvents(process.stdin);
+  if (process.stdin.isTTY) {
+    process.stdin.setRawMode(true);
+  }
+  process.stdin.on("keypress", (str, key) => {
+    if (key.ctrl && key.name === "c") {
+      process.exit(0);
+    }
+    if (key.name === "up") {
+      connection.sendUTF("up");
+      console.log("coffee status: " + true);
+    }
+    if (key.name === "down") {
+      connection.sendUTF("down");
+      console.log("coffee status: " + false);
+    }
+  });
+
+  console.log("WebSocket Client Connected");
+  connection.on("error", function (error: any) {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on("close", function () {
+    console.log("echo-protocol Connection Closed");
+  });
+  connection.on("message", function (message: any) {
+    if (message.type === "utf8") {
+      console.log("Received: '" + message.utf8Data + "'");
+    }
+  });
+
+  function sendNumber() {
+    if (connection.connected) {
+      var number = Math.round(Math.random() * 0xffffff);
+      connection.sendUTF(number.toString());
+      setTimeout(sendNumber, 1000);
+    }
+  }
+  sendNumber();
+});
+
+client.connect("ws://144.24.199.103:8999", "echo-protocol");
